b4bc7d14ed5e0c3dcd5a6ce964d16f9beec39dd8
lakruone <lakruone@gmail.com>
2024-04-09 14:37:36 +0530

    highchart packages added

--- src/components/GraphComponent.tsx
+++ src/components/GraphComponent.tsx
@@ -1,9 +1,48 @@
 import { Box } from '@mui/material'
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official'
+
+const productLabels = ['Arsenal', 'Chelsea']
+const graphData = [{
+    name: 'Point 1',
+    y: 2,
+}, {
+    name: 'Point 2',
+    y: 5,
+}]
+
+const options = {
+    title: {
+      text: 'Products in selected Category',
+      align: 'left'
+    },
+    chart: {
+        type: 'column',
+    },
+    yAxis: {
+        title: {
+            text: ''
+        }
+    },
+    xAxis: {
+        title: {
+            text: 'Smartphones'
+        },
+        categories: productLabels
+    },
+    series: [{
+        data: graphData
+    }],
+    
+  }
 
 const GraphComponent = () => {
   return (
-    <Box width={'100%'} display="flex" alignItems="center" justifyContent="center">
-        GraphComponent
+    <Box width={'100%'} display="flex" alignItems="center" justifyContent="center" >
+        <HighchartsReact
+            highcharts={Highcharts}
+            options={options}
+        />
     </Box>
   )
 }
