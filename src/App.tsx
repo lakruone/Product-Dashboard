6b218094724b5558bfc5cb3c8ba7195aea9d64b2
lakruone <lakruone@gmail.com>
2024-04-09 11:50:19 +0530

    basic project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { TestFunc } from './components/TestFunc'
+
+function App() {
+  return (
+    <>
+      <TestFunc/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TestFunc.tsx
@@ -0,0 +1,10 @@
+
+export const TestFunc = () => {
+  return (
+    <div>
+        <div>add left side filter panel</div>
+        <div>add right side graph</div>
+    </div>
+
+  )
+}
